c0021b2ecc15ea44d47d2092381e18b051602dc5
Aleywho <alexcurot@hotmail.com>
2018-11-27 19:57:17 +0100

    update fin

--- templates/delete_Article.php
+++ templates/delete_Article.php
@@ -1,4 +1,3 @@
-
 <?php require('header.php'); ?>
 
         <h2>Supprimer l'article</h2>
@@ -6,13 +5,4 @@
 
 <input type="submit" name="submit"/>
 <a href="index.php?route=admin">Retour à la page d'admin</a>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
     <?php require('footer.php'); ?>
